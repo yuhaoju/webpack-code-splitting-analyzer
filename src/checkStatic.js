9d6d2b0e592c4a436cdc6cf8dffb1d9b3f7f2232
Yuhao Ju <yuhao@tubi.tv>
2021-01-09 14:00:45 +0800

    chore: add check static script

---
+++ src/checkStatic.js
@@ -0,0 +1,78 @@
+/* eslint no-console: 0 */
+
+const fs = require('fs');
+
+const getCommitTime = (line) => {
+  const commitTimeReg = /\s(\d{10})\s[+-]\d{4}\s/; // We only need \d{10} here, wrapped with other stuff to make sure we get the correct content
+  const commitTimeMatches = line.match(commitTimeReg);
+  return commitTimeMatches ? Number(commitTimeMatches[1]) * 1000 : null;
+};
+
+const getPackageName = (line) => {
+  const packageNameReg = /\)\s+"(.+)":\s"/; // We only need (.+) here, wrapped with other stuff to make sure we get the correct content
+  const packageNameMatches = line.match(packageNameReg);
+  return packageNameMatches ? packageNameMatches[1] : null;
+};
+
+const getStaticPackages = (lines) => {
+  let reachDependenciesStart = false;
+  const staticPackages = {};
+
+  const filterByYear = (commitTime, line, maxYear) => {
+    for (let i = maxYear; i > 0; i--) {
+      const packageName = getPackageName(line);
+      if (!packageName || packageName.startsWith('@types/')) break;
+
+      const comparedYearTime = new Date().getTime() - 1000 * 60 * 60 * 24 * 365 * i;
+      const timeRange = `${i} year${i === 1 ? '' : 's'}`;
+      if(commitTime < comparedYearTime) {
+        if (!staticPackages[timeRange]) staticPackages[timeRange] = [];
+        if (packageName) staticPackages[timeRange].push(packageName);
+        break;
+      }
+    }
+  };
+
+  for (let i = 0; i < lines.length; i++) {
+    // find the beginning of dependencies block
+    const line = lines[i];
+    if (line.endsWith('"dependencies": {')) {
+      reachDependenciesStart = true;
+    }
+    if (!reachDependenciesStart) continue;
+
+    // dependencies block ends
+    if (reachDependenciesStart && line.endsWith('},')) {
+      break;
+    }
+
+    // handle content in the dependencies block
+    const commitTime = getCommitTime(line);
+    const maxYear = 5;
+    if (commitTime) {
+      filterByYear(commitTime, line, maxYear);
+    }
+  }
+
+  return staticPackages;
+};
+
+const start = () => {
+  // TODO: Generate git-blame.txt by this script.
+
+  const gitBlameContent = fs.readFileSync('./test/git-blame.txt', 'utf-8');
+  const lines = gitBlameContent.split('\n');
+  const staticPackages = getStaticPackages(lines);
+
+  let suggestion = '';
+  Object.keys(staticPackages).sort().forEach((timeRange) => {
+    const packageNames = staticPackages[timeRange];
+    if (packageNames.length > 0) {
+      suggestion += `The following module(s) have not been upgraded their versions for ${timeRange}:\n` +
+                  packageNames.map((name) => name).join('\n') + '\n';
+    }
+  });
+  console.log(suggestion);
+};
+
+start();
